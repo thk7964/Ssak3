03ce15c3e79d6b7aeca0cb97bd14d1cc426dfb86
thk7964 <yjs7894@gmail.com>
2026-01-13 21:17:42 +0900

    Initial commit

---
+++ src/main/java/com/example/ssak3/Ssak3Application.java
@@ -0,0 +1,13 @@
+package com.example.ssak3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssak3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssak3Application.class, args);
+    }
+
+}

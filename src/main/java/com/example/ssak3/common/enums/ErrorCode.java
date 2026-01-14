c5a758cd02619d940e2e24c195ef0a2e1d927f78
bjh8130 <bjh8130@gmail.com>
2026-01-14 14:20:52 +0900

    style: 에러코드 수정
    - 에러코드 수정

--- src/main/java/com/example/ssak3/common/enums/ErrorCode.java
+++ src/main/java/com/example/ssak3/common/enums/ErrorCode.java
@@ -9,7 +9,6 @@ public enum ErrorCode {
     // User 에러
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 가입된 이메일입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
     PRODUCT_ALREADY_EXISTED(HttpStatus.ALREADY_REPORTED, "이미 존재하는 상품명입니다.")
     ;
 

33344a046325564e0372bad2598fcdad87b38368
jangse0 <syjang1016@korea.ac.kr>
2026-01-15 14:23:31 +0900

    feat: cart 부분 유저 추가
    - CartController
    - CartProductController

--- src/main/java/com/example/ssak3/domain/cart/Controller/CartController.java
+++ src/main/java/com/example/ssak3/domain/cart/Controller/CartController.java
@@ -1,10 +1,12 @@
 package com.example.ssak3.domain.cart.Controller;
 
 import com.example.ssak3.common.model.ApiResponse;
+import com.example.ssak3.common.model.AuthUser;
 import com.example.ssak3.domain.cart.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +22,10 @@ public class CartController {
      * 내 장바구니 조회 API
      */
     @GetMapping
-    public ResponseEntity<ApiResponse> getMyCartApi() {
+    public ResponseEntity<ApiResponse> getMyCartApi(
+            @AuthenticationPrincipal AuthUser user) {
 
-        // 토큰이 없어서 임시 유저를 사용합니다. 추후 수정 예정
-        ApiResponse response = ApiResponse.success("장바구니 조회에 성공했습니다.", cartService.getMyCart(1L));
+        ApiResponse response = ApiResponse.success("장바구니 조회에 성공했습니다.", cartService.getMyCart(user.getId()));
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
--- src/main/java/com/example/ssak3/domain/cartproduct/controller/CartProductController.java
+++ src/main/java/com/example/ssak3/domain/cartproduct/controller/CartProductController.java
@@ -1,6 +1,7 @@
 package com.example.ssak3.domain.cartproduct.controller;
 
 import com.example.ssak3.common.model.ApiResponse;
+import com.example.ssak3.common.model.AuthUser;
 import com.example.ssak3.domain.cartproduct.model.request.CartProductAddRequest;
 import com.example.ssak3.domain.cartproduct.model.request.CartProductDeleteRequest;
 import com.example.ssak3.domain.cartproduct.model.request.CartProductQuantityUpdateRequest;
@@ -8,6 +9,7 @@ import com.example.ssak3.domain.cartproduct.service.CartProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,26 +20,31 @@ public class CartProductController {
     private final CartProductService cartProductService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse> addCartProductAPI(@RequestBody CartProductAddRequest request) {
+    public ResponseEntity<ApiResponse> addCartProductAPI(
+            @AuthenticationPrincipal AuthUser user,
+            @RequestBody CartProductAddRequest request) {
 
-        // 토큰이 없어서 userId값을 1로 고정. 토큰 추가 후 수정 예정
-        ApiResponse response = ApiResponse.success("장바구니 상품 담기에 성공했습니다.", cartProductService.addCartProduct(1L, request));
+        ApiResponse response = ApiResponse.success("장바구니 상품 담기에 성공했습니다.", cartProductService.addCartProduct(user.getId(), request));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PatchMapping
-    public ResponseEntity<ApiResponse> updateCartProductQuantityAPI(@RequestBody CartProductQuantityUpdateRequest request) {
+    public ResponseEntity<ApiResponse> updateCartProductQuantityAPI(
+            @AuthenticationPrincipal AuthUser user,
+            @RequestBody CartProductQuantityUpdateRequest request) {
 
-        ApiResponse response = ApiResponse.success("장바구니 상품 수량 변경에 성공했습니다.", cartProductService.updateCartProductQuantity(1L, request));
+        ApiResponse response = ApiResponse.success("장바구니 상품 수량 변경에 성공했습니다.", cartProductService.updateCartProductQuantity(user.getId(), request));
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @DeleteMapping
-    public ResponseEntity<ApiResponse> deleteCartProductAPI(@RequestBody CartProductDeleteRequest request) {
+    public ResponseEntity<ApiResponse> deleteCartProductAPI(
+            @AuthenticationPrincipal AuthUser user,
+            @RequestBody CartProductDeleteRequest request) {
 
-        ApiResponse response = ApiResponse.success("장바구니 상품 삭제 성공했습니다.", cartProductService.deleteCartProduct(1L, request));
+        ApiResponse response = ApiResponse.success("장바구니 상품 삭제 성공했습니다.", cartProductService.deleteCartProduct(user.getId(), request));
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }

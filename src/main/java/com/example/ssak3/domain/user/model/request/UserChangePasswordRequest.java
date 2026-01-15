ad61ad6ee46f040e592bf1e35c88a8b7fdfe85dd
jiwon jung <wldnjs2277@gmail.com>
2026-01-15 15:30:06 +0900

    fix: 비밀번호 validation 추가

--- src/main/java/com/example/ssak3/domain/user/model/request/UserChangePasswordRequest.java
+++ src/main/java/com/example/ssak3/domain/user/model/request/UserChangePasswordRequest.java
@@ -1,9 +1,11 @@
 package com.example.ssak3.domain.user.model.request;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UserChangePasswordRequest {
 
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}\\[\\]|;:'\",.<>/?])[a-z\\d!@#$%^&*()_+\\-={}\\[\\]|;:'\",.<>/?]{8,16}$", message = "비밀번호는 8자 이상 16자 이하 영문 소문자, 숫자, 특수 문자를 포함해야 합니다.")
     private String newPassword;
 }

d355cd144990b5d963fc62e01fc5010528d46127
jiwon jung <wldnjs2277@gmail.com>
2026-01-15 17:37:03 +0900

    fix: 누락 내용 수정
    - 정적 팩토리 메소드 from 추가
    - 누락된 PASSWORD_MISMATCH enum 추가

--- src/main/java/com/example/ssak3/common/enums/ErrorCode.java
+++ src/main/java/com/example/ssak3/common/enums/ErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // User 에러
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 가입된 이메일입니다."),
     NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
     UNREGISTERED_USER(HttpStatus.NOT_FOUND, "가입되지 않은 사용자입니다."),

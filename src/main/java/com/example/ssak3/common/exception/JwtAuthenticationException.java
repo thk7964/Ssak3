2f3050657b1c26735faf9bd73c3c5347e2666373
jiwon jung <wldnjs2277@gmail.com>
2026-01-16 22:08:20 +0900

    style: 코드 컨벤션 준수

--- src/main/java/com/example/ssak3/common/exception/JwtAuthenticationException.java
+++ src/main/java/com/example/ssak3/common/exception/JwtAuthenticationException.java
@@ -4,7 +4,5 @@ import org.springframework.security.core.AuthenticationException;
 
 public class JwtAuthenticationException extends AuthenticationException {
 
-    public JwtAuthenticationException(String message) {
-        super(message);
-    }
+    public JwtAuthenticationException(String message) { super(message); }
 }

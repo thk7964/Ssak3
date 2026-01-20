7b1dd3c180b33a6868d158ae9828d9e0ccbcc6bb
jiwon jung <wldnjs2277@gmail.com>
2026-01-20 21:08:19 +0900

    fix: QueryDSL을 이용해 유저 목록 조회 시 닉네임으로 검색 가능하도록 수정

--- src/main/java/com/example/ssak3/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/ssak3/domain/user/repository/UserRepository.java
@@ -1,19 +1,11 @@
 package com.example.ssak3.domain.user.repository;
 
-import com.example.ssak3.common.enums.UserRole;
 import com.example.ssak3.domain.user.entity.User;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserCustomRepository {
 
     boolean existsByEmail(String email);
 
@@ -24,11 +16,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailAndIsDeletedFalse(String email);
 
     Optional<User> findByIdAndIsDeletedFalse(Long id);
-
-    @Query("""
-        SELECT u
-        FROM User u
-        WHERE u.isDeleted = :isDeleted AND u.role = :role
-        """)
-    Page<User> findUserByRole(@Param("role") UserRole role, @Param("isDeleted") boolean isDeleted, Pageable pageable);
 }

ecf67aea2fa2b68c4f7839a9fb7186638860d9dc
jiwon jung <wldnjs2277@gmail.com>
2026-01-29 19:10:44 +0900

    feat: 조회수 TOP 10 상품 조회 주간 랭킹 집계로 변경

--- src/main/java/com/example/ssak3/domain/product/service/ProductRankingService.java
+++ src/main/java/com/example/ssak3/domain/product/service/ProductRankingService.java
@@ -13,6 +13,8 @@ import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -22,26 +24,71 @@ public class ProductRankingService {
 
     private final StringRedisTemplate redisTemplate;
     private static final String PRODUCT_DAILY_RANKING_PREFIX = "product:ranking:";
+    private static final String PRODUCT_WEEKLY_RANKING_KEY = "product:ranking:weekly";
     private final ProductRepository productRepository;
     private final TimeDealRepository timeDealRepository;
 
+    /**
+     * 조회수 증가 메소드
+     */
     public void increaseViewCount(Long productId) {
 
-        redisTemplate.opsForZSet().incrementScore(PRODUCT_DAILY_RANKING_PREFIX + LocalDate.now(), productId.toString(), 1);
+        LocalDate now = LocalDate.now();
+
+        Double score = redisTemplate.opsForZSet().incrementScore(PRODUCT_DAILY_RANKING_PREFIX + now, productId.toString(), 1);
+
+        // 최초 한 번만 TTL 설정
+        if (score != null && score == 1.0) {
+
+            // 오늘로부터 10일 뒤 자정 시점 구하기
+            LocalDateTime expirationTime = now.plusDays(10).atStartOfDay();
+
+            // TTL 설정: Redis에서 오늘로부터 10일 뒤 데이터 만료
+            redisTemplate.expireAt(PRODUCT_DAILY_RANKING_PREFIX + now, expirationTime.atZone(ZoneId.systemDefault()).toInstant());
+        }
+
     }
 
+    /**
+     * 조회수 TOP 10 상품 조회
+     */
     public List<ProductGetPopularResponse> getPopularProductTop10() {
 
-        Set<ZSetOperations.TypedTuple<String>> result = redisTemplate.opsForZSet().reverseRangeWithScores(PRODUCT_DAILY_RANKING_PREFIX + LocalDate.now(), 0, 9);
+        LocalDate now = LocalDate.now();
+
+        List<String> otherKeys = new ArrayList<>();
+
+        // 현재 시점으로부터 일주일치 키 리스트 생성 (오늘은 제외)
+        for (int i = 0; i < 6; i++) {
+            otherKeys.add(PRODUCT_DAILY_RANKING_PREFIX + now.minusDays(i + 1));
+        }
+
+        // 일주일치 결과 집계
+        redisTemplate.opsForZSet().unionAndStore(
+                PRODUCT_DAILY_RANKING_PREFIX + now,
+                    otherKeys,
+                    PRODUCT_WEEKLY_RANKING_KEY);
+
+        // 오늘로부터 10일 뒤 자정 시점 구하기
+        LocalDateTime expirationTime = now.plusDays(10).atStartOfDay();
+
+        // TTL 설정: 덮어씌워질 때마다 만료 시간을 10일 뒤로 갱신
+        redisTemplate.expireAt(PRODUCT_WEEKLY_RANKING_KEY, expirationTime.atZone(ZoneId.systemDefault()).toInstant());
+
+        // 랭킹으로 조회
+        Set<ZSetOperations.TypedTuple<String>> result = redisTemplate.opsForZSet().reverseRangeWithScores(PRODUCT_WEEKLY_RANKING_KEY, 0, 9);
 
         if (result == null || result.isEmpty()) {
             return Collections.emptyList();
         }
 
+        // DB에서 TOP 10 상품 id 리스트 가져오기: Redis가 정렬해준 순서 보장함
         List<Long> productIdList = result.stream().map(tuple -> Long.parseLong(tuple.getValue())).toList();
 
+        // DB에서 Product 가져오기: in 연산은 Redis가 정렬해준 순서를 보장해주지 않음
         List<Product> productList = productRepository.findAllByIdInAndStatusAndIsDeletedFalse(productIdList, ProductStatus.FOR_SALE);
 
+        // DB에서 TimeDeal 중인 상품 있으면 가져오기
         List<TimeDeal> timeDealList = timeDealRepository.findAllByProductIdInAndStatusAndIsDeletedFalse(productIdList, TimeDealStatus.OPEN);
 
         Map<Long, Product> productMap = productList.stream()
@@ -50,12 +97,13 @@ public class ProductRankingService {
         Map<Long, TimeDeal> timeDealMap = timeDealList.stream()
                 .collect(Collectors.toMap(timeDeal -> timeDeal.getProduct().getId(), timeDeal -> timeDeal));
 
+        // 랭킹에 따라 조립
         return productIdList.stream()
                 .map(id -> {
                     Product product = productMap.get(id);
                     return product != null ? ProductGetPopularResponse.from(product, timeDealMap.get(id)) : null;
                 })
-                .filter(Objects::nonNull)
+                .filter(Objects::nonNull) // null인 애들은 걸러냄
                 .toList();
     }
 

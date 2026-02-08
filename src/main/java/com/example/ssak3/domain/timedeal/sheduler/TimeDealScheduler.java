228e26f94608dbe704c4affbb57bc3e08ead56ff
thk7964 <yjs7894@gmail.com>
2026-02-09 02:40:53 +0900

    test: 타임딜 test를 위한 임시 변경

--- src/main/java/com/example/ssak3/domain/timedeal/sheduler/TimeDealScheduler.java
+++ src/main/java/com/example/ssak3/domain/timedeal/sheduler/TimeDealScheduler.java
@@ -3,7 +3,6 @@ package com.example.ssak3.domain.timedeal.sheduler;
 import com.example.ssak3.common.enums.TimeDealStatus;
 import com.example.ssak3.domain.timedeal.entity.TimeDeal;
 import com.example.ssak3.domain.timedeal.repository.TimeDealRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
@@ -11,7 +10,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.net.InetAddress;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -22,19 +20,7 @@ public class TimeDealScheduler {
 
     private final TimeDealRepository timeDealRepository;
 
-    /** 로그용 인스턴스 식별자 */
-    private String instanceId;
-
-    @PostConstruct
-    void init() {
-        try {
-            this.instanceId = InetAddress.getLocalHost().getHostName();
-        } catch (Exception e) {
-            this.instanceId = "unknown-host";
-        }
-    }
-
-    @Scheduled(cron = "0 0/5 * * * *")
+    @Scheduled(cron = "0 0 * * * *")
     @SchedulerLock(
             name = "timeDealScheduler",
             lockAtMostFor = "PT1M",
@@ -43,18 +29,15 @@ public class TimeDealScheduler {
     @Transactional
     public void updateTimeDealStatus() {
         LocalDateTime now = LocalDateTime.now();
-        log.info("[{}] TimeDeal Scheduler START at {}", instanceId, now);
 
         List<TimeDeal> open = timeDealRepository.findReadyToOpen(now);
         for (TimeDeal timeDeal : open) {
             timeDeal.setStatus(TimeDealStatus.OPEN);
-            log.info("[{}] OPEN dealId={}", instanceId, timeDeal.getId());
         }
 
         List<TimeDeal> close = timeDealRepository.findOpenToClose(now);
         for (TimeDeal timeDeal : close) {
             timeDeal.setStatus(TimeDealStatus.CLOSED);
-            log.info("[{}] CLOSE dealId={}", instanceId, timeDeal.getId());
         }
 
     }

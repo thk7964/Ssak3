b1cfa5f03039b28b3bc64525a482742bf26da463
jiwon jung <wldnjs2277@gmail.com>
2026-02-12 10:07:41 +0900

    fix: 생일은 현재 시점보다 늦을 수 없게 수정

--- src/main/java/com/example/ssak3/domain/auth/service/AuthService.java
+++ src/main/java/com/example/ssak3/domain/auth/service/AuthService.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+
 @Service
 @RequiredArgsConstructor
 public class AuthService {
@@ -40,6 +42,10 @@ public class AuthService {
 
         if (userRepository.existsByPhone(request.getPhone())) {
             throw new CustomException(ErrorCode.PHONE_ALREADY_EXISTS);
+        }
+
+        if (request.getBirth().isAfter(LocalDate.now())) {
+            throw new CustomException(ErrorCode.INVALID_BIRTH);
         }
 
         User user = new User(

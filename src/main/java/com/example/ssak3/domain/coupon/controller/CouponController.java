1bf2374b4445756e3b2e57911ee2ced9e06ae11b
JH319 <imjh3205@gmail.com>
2026-02-12 09:27:18 +0900

    Update CouponController.java
    admin-coupons → coupons 로 수정

--- src/main/java/com/example/ssak3/domain/coupon/controller/CouponController.java
+++ src/main/java/com/example/ssak3/domain/coupon/controller/CouponController.java
@@ -15,7 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ssak3/admin/admin-coupons")
+@RequestMapping("/ssak3/admin/coupons")
 @RequiredArgsConstructor
 public class CouponController {
 

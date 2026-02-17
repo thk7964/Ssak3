210b92ca87492add1a3fa45db3e1672e31d5ad1e
thk7964 <yjs7894@gmail.com>
2026-02-17 17:58:39 +0900

    refactor: 쿠폰 중복 이름 및 할인 금액 정책에 따라 수정

--- src/main/java/com/example/ssak3/common/enums/ErrorCode.java
+++ src/main/java/com/example/ssak3/common/enums/ErrorCode.java
@@ -73,6 +73,7 @@ public enum ErrorCode {
     // Coupon 에러
     COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 쿠폰입니다."),
     COUPON_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 지급된 쿠폰입니다."),
+    COUPON_NAME_ALREADY_EXISTS(HttpStatus.CONFLICT,"중복된 쿠폰 이름입니다." ),
     COUPON_OUT_OF_STOCK(HttpStatus.CONFLICT, "쿠폰 수량이 모두 소진되었습니다."),
     COUPON_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "이미 삭제된 쿠폰입니다."),
     COUPON_INVALID_TIME_RANGE(HttpStatus.BAD_REQUEST, "종료일은 시작일 이전일 수 없습니다."),
@@ -80,6 +81,7 @@ public enum ErrorCode {
     COUPON_INVALID_END_TIME(HttpStatus.BAD_REQUEST, "종료일은 현재 시간 이전일 수 없습니다."),
     COUPON_MIN_ORDER_PRICE_NOT_MET(HttpStatus.BAD_REQUEST, "최소 주문 금액을 만족하지 못해 쿠폰을 사용할 수 없습니다."),
     COUPON_NOT_AVAILABLE(HttpStatus.BAD_REQUEST, "사용 가능한 상태의 쿠폰이 아닙니다."),
+    DISCOUNT_EXCEEDS_MIN_ORDER_PRICE(HttpStatus.BAD_REQUEST, "할인 금액은 최소 금액보다 클 수 없습니다."),
     ADMIN_CANNOT_ISSUE_COUPON(HttpStatus.FORBIDDEN, "관리자는 쿠폰을 발급받을 수 없습니다."),
     FORBIDDEN_COUPON_ACCESS(HttpStatus.FORBIDDEN, "본인의 쿠폰만 사용할 수 있습니다."),
 

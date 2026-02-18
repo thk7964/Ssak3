b51d1f51b064320515f0988d6f3cb4ade0a81326
LemonCoding99 <qordmstj7530@gmail.com>
2026-02-19 00:46:17 +0900

    refactor: 공백 리팩토링

--- src/main/java/com/example/ssak3/domain/inquiryreply/repository/InquiryReplyRepository.java
+++ src/main/java/com/example/ssak3/domain/inquiryreply/repository/InquiryReplyRepository.java
@@ -2,6 +2,7 @@ package com.example.ssak3.domain.inquiryreply.repository;
 
 import com.example.ssak3.domain.inquiryreply.entity.InquiryReply;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface InquiryReplyRepository extends JpaRepository<InquiryReply, Long> {

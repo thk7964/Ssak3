2c8a1489f3362f9678e87f68be3ca25962713d4f
LemonCoding99 <qordmstj7530@gmail.com>
2026-02-18 23:18:03 +0900

    refactor: 코드 정리

--- src/main/java/com/example/ssak3/domain/inquiry/repository/InquiryRepository.java
+++ src/main/java/com/example/ssak3/domain/inquiry/repository/InquiryRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.Optional;
 
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
@@ -23,5 +22,4 @@ public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
     Page<Inquiry> findAllByIsDeletedFalse(Pageable pageable);
 
     Optional<Inquiry> findByIdAndIsDeletedFalse(Long inquiryId);
-
 }
